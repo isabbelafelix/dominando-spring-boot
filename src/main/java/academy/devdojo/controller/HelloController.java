0eef2843768f4ca99892195eda37abde41caa204
Isabela Felix <isabbelafelix@gmail.com>
2023-06-13 22:06:25 -0300

    video05: explain get method, and RestController

---
+++ src/main/java/academy/devdojo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package academy.devdojo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "greetings")
+public class HelloController {
+
+    @GetMapping("hi")
+    public String hi(){
+        return "OMAE WA MOU SHINDEIRU";
+
+    }
+
+}
