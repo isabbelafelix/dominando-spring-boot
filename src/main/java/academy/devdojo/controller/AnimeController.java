b067d3d1f94c40ad697d2c841085ce3db5367ff7
Isabela Felix <isabbelafelix@gmail.com>
2023-06-15 21:42:19 -0300

    video11: add lombok

--- src/main/java/academy/devdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,16 +1,19 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Anime;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping(path = "v1/animes")
 public class AnimeController {
 
     @GetMapping("")
     public List<Anime> listAllAnimes(@RequestParam(required = false) String name){
+        log.info("Request received to list all animes, param name '{}'", name);
         var animes = Anime.getAnimes();
         if (name == null) return animes;
         return animes.stream().filter(anime -> anime.getName().equalsIgnoreCase(name)).toList();
@@ -18,6 +21,7 @@ public class AnimeController {
 
     @GetMapping("{id}")
     public Anime findById(@PathVariable Long id){
+        log.info("Request received find anime by id '{}'", id);
         return Anime.getAnimes()
                 .stream()
                 .filter(anime -> anime.getId().equals(id))
--- src/main/java/academy/devdojo/domain/Anime.java
+++ src/main/java/academy/devdojo/domain/Anime.java
@@ -1,31 +1,15 @@
 package academy.devdojo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class Anime {
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Anime(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public static List<Anime> getAnimes(){
         var jigokuraku = new Anime(1L, "jigokuraku");
