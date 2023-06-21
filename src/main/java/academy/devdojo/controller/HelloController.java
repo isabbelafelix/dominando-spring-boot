6e487d3032eb42095a913df87a92832e8dab1507
Isabela Felix <isabbelafelix@gmail.com>
2023-06-20 21:18:00 -0300

    video12: postMapping

--- src/main/java/academy/devdojo/controller/HelloController.java
+++ src/main/java/academy/devdojo/controller/HelloController.java
@@ -1,17 +1,25 @@
 package academy.devdojo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping(path = "greetings")
+@Log4j2
 public class HelloController {
 
     @GetMapping("hi")
     public String hi(){
         return "OMAE WA MOU SHINDEIRU";
 
+    }
+
+    @PostMapping
+    public Long save(@RequestParam String name){
+        log.info("Saving name '{}'", name);
+        return ThreadLocalRandom.current().nextLong();
     }
 
 }
